9b9f2168bce6b687de2eab89058cb2315fdd9ec5
Moshe Lieberman <moshe@fiverr.com>
2013-07-23 11:15:20 +0300

    [TEST] removing lacmus::lab namespcae, not needed anymore because its now a mixin

--- spec/lab_spec.rb
+++ spec/lab_spec.rb
@@ -61,12 +61,12 @@ describe Lacmus::Lab, "Lab" do
   it "should render different results for control and experiment groups (string)" do
     experiment_id = create_and_activate_experiment
     result1 = simple_experiment(experiment_id, "control", "experiment")
-    expect(Lacmus::Lab.user_belongs_to_control_group?).to be_false
+    expect(user_belongs_to_control_group?).to be_false
     expect(result1).to eq("experiment")
-    Lacmus::Lab.clear_cookies
+    clear_cookies
 
-    result2 = Lacmus::Lab.simple_experiment(experiment_id, "control", "experiment")
-    expect(Lacmus::Lab.user_belongs_to_control_group?).to be_true
+    result2 = simple_experiment(experiment_id, "control", "experiment")
+    expect(user_belongs_to_control_group?).to be_true
     expect(result2).to eq("control")
   end
 
@@ -75,25 +75,25 @@ describe Lacmus::Lab, "Lab" do
   	block1 = Proc.new {|i| "text for block1"}
   	block2 = Proc.new {|i| "text for block2"}
 
-		expect(Lacmus::Lab.user_belongs_to_control_group?).to be_false
-  	result1 = Lacmus::Lab.render_control_version(experiment_id, &block1)
+		expect(user_belongs_to_control_group?).to be_false
+  	result1 = render_control_version(experiment_id, &block1)
 		expect(result1).to be_nil
 
-  	result2 = Lacmus::Lab.render_experiment_version(experiment_id, &block2)
+  	result2 = render_experiment_version(experiment_id, &block2)
   	expect(result2).to eq("text for block2")
-  	Lacmus::Lab.clear_cookies
+  	clear_cookies
 
-		expect(Lacmus::Lab.user_belongs_to_control_group?).to be_true
-  	result3 = Lacmus::Lab.render_control_version(experiment_id, &block1)
+		expect(user_belongs_to_control_group?).to be_true
+  	result3 = render_control_version(experiment_id, &block1)
   	expect(result3).to eq("text for block1")
 
-  	result4 = Lacmus::Lab.render_experiment_version(experiment_id, &block2)
+  	result4 = render_experiment_version(experiment_id, &block2)
   	expect(result4).to be_nil
   end
 
   it "should render control group if experiment isn't active (string)" do
   	experiment_id = create_experiment
-  	result = Lacmus::Lab.simple_experiment(experiment_id, "control", "experiment")
+  	result = simple_experiment(experiment_id, "control", "experiment")
   	expect(result).to eq("control")
   end
 
@@ -101,24 +101,24 @@ describe Lacmus::Lab, "Lab" do
 		experiment_id = create_experiment
 		block = Proc.new {|i| "text for block"}
 
-		expect(Lacmus::Lab.user_belongs_to_control_group?).to be_false
-		result1 = Lacmus::Lab.render_control_version(experiment_id, &block)
+		expect(user_belongs_to_control_group?).to be_false
+		result1 = render_control_version(experiment_id, &block)
 		expect(result1).to eq("text for block")
 
-  	result2 = Lacmus::Lab.render_experiment_version(experiment_id, &block)
+  	result2 = render_experiment_version(experiment_id, &block)
   	expect(result2).to be_nil
   end
 
   # Moshe to Shai: probably not what you wanted to see here...
   it "should render experiment before and after cookie expired (string)" do
   	experiment_id = create_and_activate_experiment
-  	expect(Lacmus::Lab.user_belongs_to_control_group?).to be_false
+  	expect(user_belongs_to_control_group?).to be_false
 
-  	result = Lacmus::Lab.simple_experiment(experiment_id, "control", "experiment")
+  	result = simple_experiment(experiment_id, "control", "experiment")
   	expect(result).to eq("experiment")
-  	Lacmus::Lab.clear_cookies
+  	clear_cookies
 
-  	result2 = Lacmus::Lab.simple_experiment(experiment_id, "control", "experiment")
+  	result2 = simple_experiment(experiment_id, "control", "experiment")
   	expect(result2).to eq("control")
   end
 
