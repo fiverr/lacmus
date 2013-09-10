4d6bdc772daf679215b0aa1655d59fca05beada3
Moshe Lieberman <moshe@fiverr.com>
2013-09-10 19:45:13 +0200

    [TEST] adding tests to alternative user functionality

---
+++ spec/alternative_user_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'lacmus'
+
+include Lacmus::Lab
+
+describe Lacmus::AlternativeUser, 'AlternativeUser' do
+
+  before do
+		clear_cookies	
+		Lacmus.restart_user_ids_counter
+  	Lacmus::Experiment.nuke_all_experiments
+	end
+
+  describe 'Basic functionality' do
+		
+		before do
+			set_alternative_user_id(700)
+		end  	
+
+  	it 'should modify the user id cookie' do
+  		expect(user_id_cookie[:value].split('|').last).to eq('1')
+  	end
+
+  	it 'should return the lacmus user id' do
+  		expect(Lacmus::AlternativeUser.get_user_id(700)).to eq('1')
+  	end
+
+  end
+end
