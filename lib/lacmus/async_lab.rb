263ea098082714368cb1722eff37d15e1cb18927
Moshe Lieberman <moshe@fiverr.com>
2013-09-10 12:13:39 +0200

    [FEAT] if lacmus user id wasnt found using the alternative user id - we can't mark the kpi..

--- lib/lacmus/async_lab.rb
+++ lib/lacmus/async_lab.rb
@@ -1,15 +1,20 @@
 module Lacmus
 	module AsyncLab
-		extend Lab
 		extend self
+		extend Lab
 
-		class NotImplemented < StandardError; end #:nodoc:
+		class NotImplemented < StandardError; end
 
 		attr_accessor :__lcm__cached_user_id
 
 		def mark_kpi!(kpi, alternative_user_id)
 			user_id = AlternativeUser.get_user_id(alternative_user_id)
-			self.__lcm__cached_user_id = user_id
+			unless user_id
+				lacmus_logger "Can't mark async kpi: #{kpi} because lacmus id" <<
+										  "wasn't found for #{alternative_user_id}" and return
+			end
+
+			self.__lcm__cached_user_id = user_id.to_i
 			super(kpi)
 		end
 
